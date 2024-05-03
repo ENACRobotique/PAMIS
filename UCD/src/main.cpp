04cafc6da9c067e1a300fcdc237734999fe766fe
ENAC-Robotique <robotique.enac@gmail.com>
2024-05-03 14:57:41 +0200

    UCD again

--- UCD/src/main.cpp
+++ UCD/src/main.cpp
@@ -3,20 +3,65 @@
 #include <math.h>
 #include <stdio.h>
 
-#define LED PA3
+#define LED1 PA3
+#define LED2 PA1
+#define LED3 PA0
 #define TIRETTE PA5
+
+enum state{
+    DEBUT,
+    TIRETTE_A_REMETTRE,
+    PRET_DEPART,
+    DEPART
+};
+
+state etat = DEBUT;
+
 
 void setup (){
     Serial.begin(115200);
-    pinMode(TIRETTE, OUTPUT);
-    pinMode(LED, OUTPUT);
+    pinMode(TIRETTE, INPUT_PULLUP);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
 }
 
 
-void loop(){
-    digitalWrite(TIRETTE, HIGH);
-    if (digitalRead(TIRETTE)== LOW){
-        delay(90000);
-        digitalWrite(LED, HIGH);
+void loop(){;
+    Serial.println((digitalRead(TIRETTE)));
+    switch (etat)
+    {
+    case DEBUT:
+        if (digitalRead(TIRETTE)==LOW){
+            digitalWrite(LED1, HIGH);
+            //digitalWrite(LED2, HIGH);
+            //digitalWrite(LED3, HIGH);
+            etat=TIRETTE_A_REMETTRE;
+            delay(1000);
+        }
+        break;
+    case TIRETTE_A_REMETTRE:
+        if (digitalRead(TIRETTE)==HIGH){
+            digitalWrite(LED1, LOW);
+            digitalWrite(LED2, LOW);
+            digitalWrite(LED3, LOW);
+            etat=PRET_DEPART;
+            delay(1000);
+        }
+        break;
+    case PRET_DEPART:
+        if (digitalRead(TIRETTE)==LOW){
+            //delay(90000);
+            //digitalWrite(LED1, HIGH);
+            digitalWrite(LED2, HIGH);
+            //digitalWrite(LED3, HIGH);
+            etat=DEPART;
+            delay(1000);
+        }
+        break;
     }
+
 }
