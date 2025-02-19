7d29cd6b80f46b932062e41dacaa8a3fabad80e7
ENACRobotique <robotique.enac@gmail.com>
2025-02-19 20:07:24 +0100

    projet 2025 pami platformio

---
+++ 2025/code_PAMI_2025/src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#undef LED1
+#undef LED2
+#define LED1 2
+#define LED2 3
+#define UARTTX 4
+#define UARTRX 5
+#define ENABLE1 6
+#define DIR2 8
+#define STEP2 9
+#define DIR1 0
+#define STEP1 1
+#define ENABLE2 15
+#define FDC1 17
+#define FDC2 18
+#define SHUTDOWN1 19
+#define SHUTDOWN2 22
+#define SHUTDOWN3 26
+#define SDA 20
+#define SCL 21
+#define SERVO 27
+#define BAT_MESURE 28
+
+
+void setup() {
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,HIGH);
+  delay(250);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  delay(250);
+}
+
